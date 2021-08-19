892f2c570c11d71558c28173fb358365f152567d
Ben <benjamin.sores@gmail.com>
2021-08-19 20:27:29 +0200

    modif

--- Front-end/script.js
+++ Front-end/script.js
@@ -1,3 +1,7 @@
+// const img = document.getElementById('img1');
+
 fetch ('http://localhost:3000/api/teddies')
     .then(res => res.json())
     .then(data => console.log(data))
+
+    // .then(data => img.src = data[0].url)
