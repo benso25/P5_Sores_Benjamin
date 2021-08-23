d673f42540b0b13c79eadb7a1fff22bdf6aad5ae
Ben <benjamin.sores@gmail.com>
2021-08-23 14:28:56 +0200

    suite page acceuil

--- Front-end/script.js
+++ Front-end/script.js
@@ -1,7 +1,232 @@
-// const img = document.getElementById('img1');
+// declaration des constantes pour les images 
+
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const img3 = document.getElementById('img3');
+const img4 = document.getElementById('img4');
+const img5 = document.getElementById('img5');
+
+// declaration des constantes pour les noms
+
+const name1 = document.getElementById('name1');
+const name2 = document.getElementById('name2');
+const name3 = document.getElementById('name3');
+const name4 = document.getElementById('name4');
+const name5 = document.getElementById('name5');
+
+// declaration des constantes pour les prix
+
+const price1 = document.getElementById('price1');
+const price2 = document.getElementById('price2');
+const price3 = document.getElementById('price3');
+const price4 = document.getElementById('price4');
+const price5 = document.getElementById('price5');
+
+
+// Appel des images depuis l'API
 
 fetch ('http://localhost:3000/api/teddies')
-    .then(res => res.json())
-    .then(data => console.log(data))
+    .then(res => {
+        if(res.ok){
+            res.json().then(data => {
+                img1.src = data[0].imageUrl
+            })
+        } else{
+            console.log("ERREUR");
+            document.getElementById('error1').innerHTML = "Erreur"
+        }
+    
+    })
 
-    // .then(data => img.src = data[0].url)
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            img2.src = data[1].imageUrl
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error2').innerHTML = "Erreur"
+    }
+
+})
+
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            img3.src = data[2].imageUrl
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error3').innerHTML = "Erreur"
+    }
+
+})
+
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            img4.src = data[3].imageUrl
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error4').innerHTML = "Erreur"
+    }
+
+})
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            img5.src = data[4].imageUrl
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error5').innerHTML = "Erreur"
+    }
+
+})
+
+
+// Appel des noms depuis l'API
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            name1.innerHTML = data[0].name
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error1').innerHTML = "Erreur"
+    }
+
+})
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            name2.innerHTML = data[1].name
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error2').innerHTML = "Erreur"
+    }
+
+})
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            name3.innerHTML = data[2].name
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error3').innerHTML = "Erreur"
+    }
+
+})
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            name4.innerHTML = data[3].name
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error4').innerHTML = "Erreur"
+    }
+
+})
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            name5.innerHTML = data[4].name
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error5').innerHTML = "Erreur"
+    }
+
+})
+
+//appel des prix depuis l'API
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            price1.innerHTML = data[0].price
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error1').innerHTML = "Erreur"
+    }
+
+})
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            price2.innerHTML = data[1].price
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error2').innerHTML = "Erreur"
+    }
+
+})
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            price3.innerHTML = data[2].price
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error3').innerHTML = "Erreur"
+    }
+
+})
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            price4.innerHTML = data[3].price
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error4').innerHTML = "Erreur"
+    }
+
+})
+
+fetch ('http://localhost:3000/api/teddies')
+.then(res => {
+    if(res.ok){
+        res.json().then(data => {
+            price5.innerHTML = data[4].price
+        })
+    } else{
+        console.log("ERREUR");
+        document.getElementById('error5').innerHTML = "Erreur"
+    }
+
+})
+
+
+
